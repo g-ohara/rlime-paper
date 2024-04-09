b40f945a2843db08b67985e3012c37c111abf127
Genji Ohara <oharagenji1024@gmail.com>
2024-04-09 12:09:13 +0900

    modify lime_vs_rlime.py to match updating submodule exp2/rlime

--- experiments/exp2/lime_vs_rlime.py
+++ experiments/exp2/lime_vs_rlime.py
@@ -6,21 +6,23 @@ from functools import partial
 
 import numpy as np
 import pandas as pd  # type: ignore
-from lime import lime_tabular  # type: ignore
+from river import preprocessing
 from sklearn.ensemble import RandomForestClassifier  # type: ignore
 
-from rlime import mylime, newlime_base, newlime_tabular, newlime_utils
-from rlime.newlime_base import Arm
-from rlime.newlime_types import IntArray
+from rlime import rlime_lime
+from rlime.arm import Arm
+from rlime.rlime import HyperParam, explain_instance
+from rlime.rlime_types import Dataset, IntArray
 from rlime.sampler import Sampler
+from rlime.utils import get_trg_sample, load_dataset
 
 
 def main() -> None:
     """Main function."""
 
     # Load the dataset.
-    dataset = newlime_utils.load_dataset(
-        "recidivism", "RLIME/datasets/", balance=True
+    dataset = load_dataset(
+        "recidivism", "rlime/examples/datasets/", balance=True
     )
 
     # Learn the black box model.
@@ -63,24 +65,26 @@ def main() -> None:
 
 
 def calc_accuracy(
-    sample: IntArray,
+    samples: IntArray,
     labels: IntArray,
     lime_weights: list[float],
-    lime_scaler,
+    lime_scaler: preprocessing.StandardScaler,
     arm: Arm,
 ) -> tuple[float, float]:
     """Calculate the precision of LIME and R-LIME.
 
     Parameters
     ----------
-    sample: newlime_utils.FloatArray
-        The samples.
-    labels: newlime_utils.IntArray
+    samples : IntArray
+        The samples to evaluate.
+    labels : IntArray
         The labels of the samples.
-    lime_weights: newlime_utils.FloatArray
+    lime_weights : list[float]
         The weights of LIME.
-    surrogate_model: newlime_tabular.SurrogateModel
-        The surrogate model of R-LIME.
+    lime_scaler : preprocessing.StandardScaler
+        The scaler of LIME.
+    arm : Arm
+        The rule of R-LIME.
 
     Returns
     -------
@@ -89,8 +93,8 @@ def calc_accuracy(
     """
 
     # Get predictions of LIME and R-LIME.
-    lime_pred = np.dot(lime_scaler.transform(sample), lime_weights) > 0
-    rlime_pred = arm.surrogate_model.predict_many(pd.DataFrame(sample))
+    lime_pred = np.dot(lime_scaler.transform_many(samples), lime_weights) > 0
+    rlime_pred = arm.surrogate_model.predict_many(pd.DataFrame(samples))
 
     # Calculate the precision of LIME and R-LIME.
     lime_acc = np.sum(lime_pred == labels) / len(labels)
@@ -99,32 +103,9 @@ def calc_accuracy(
     return lime_acc, rlime_acc
 
 
-def original_lime(
-    dataset: newlime_tabular.Dataset,
-    trg: IntArray,
-    model: RandomForestClassifier,
-    pred_label: int,
-) -> list[float]:
-    """Calculate the weights of LIME."""
-
-    lime_explainer = lime_tabular.LimeTabularExplainer(
-        dataset.train,
-        feature_names=dataset.feature_names,
-        class_names=dataset.class_names,
-        discretize_continuous=False,
-    )
-    lime_exp = lime_explainer.explain_instance(
-        trg, model.predict_proba, num_features=15, top_labels=1
-    )
-    weights = [0.0] * len(dataset.feature_names)
-    for t in lime_exp.local_exp[pred_label]:
-        weights[t[0]] = t[1] * (pred_label * 2 - 1)
-    return weights
-
-
 def compare_lime_and_newlime(
     idx: int,
-    dataset: newlime_tabular.Dataset,
+    dataset: Dataset,
     black_box: RandomForestClassifier,
     tau: float,
     result_list: list[tuple[float, float]],
@@ -133,61 +114,60 @@ def compare_lime_and_newlime(
 
     Parameters
     ----------
-    idx: int
+    idx : int
         The index of the target instance.
-    trg: newlime_utils.IntArray
-        The target instance.
-    dataset: newlime_utils.Dataset
+    dataset : Dataset
         The dataset.
-    black_box: RandomForestClassifier
+    black_box : RandomForestClassifier
         The black box model.
-    hyper_param: newlime_tabular.HyperParam
-        The hyperparameters of R-LIME.
+    tau : float
+        The threshold of R-LIME.
+    result_list : list[tuple[float, float]]
+        The list to store the results.
     """
 
     print(f"Process {idx:03d} Started.")
-    trg, _, _ = newlime_utils.get_trg_sample(idx, dataset)
+    trg, _, _ = get_trg_sample(idx, dataset)
 
     # Initialize sampler.
     sampler = Sampler(
         trg, dataset.train, black_box.predict, dataset.categorical_names
     )
 
-    lime_weights, scaler = mylime.explain(trg, sampler, 5000)
-    # lime_weights = original_lime(
-    #     dataset, trg, black_box, black_box.predict(trg.reshape(1, -1))[0]
-    # )
+    lime_weights, scaler = rlime_lime.explain(trg, sampler, 5000)
 
-    hyper_param = newlime_base.HyperParam(
-        tau=tau,
-        delta=0.05,
-        epsilon=0.1,
-        epsilon_stop=0.05,
-        beam_size=10,
-        batch_size=500,
-        coverage_samples_num=10000,
-        max_rule_length=None,
-    )
-    result = newlime_tabular.explain_instance(
-        trg, dataset, black_box.predict, hyper_param
-    )
-    if result is None:
-        print(f"Process {idx:03d} Found No explanations.")
-        return
+    def get_rlime_arm() -> Arm | None:
+        hyper_param = HyperParam(
+            tau=tau,
+            delta=0.05,
+            epsilon=0.1,
+            epsilon_stop=0.05,
+            beam_size=10,
+            batch_size=500,
+            coverage_samples_num=10000,
+            max_rule_length=None,
+        )
+        result = explain_instance(trg, dataset, black_box.predict, hyper_param)
+        if result is None:
+            print(f"Process {idx:03d} Found No explanations.")
+            return None
 
-    _, arm = result
+        _, arm = result
+        return arm
 
-    # Sample from the rule.
-    sample, labels = sampler.sample(10000, arm.rule)
-    lime_acc, rlime_acc = calc_accuracy(
-        sample, labels, lime_weights, scaler, arm
-    )
+    if (arm := get_rlime_arm()) is not None:
 
-    # Print and save the results.
-    print(f"Process {idx:03d} Finished.")
-    print(f"  LIME  : {lime_acc:.4f}")
-    print(f"  R-LIME: {rlime_acc:.4f}")
-    result_list.append((lime_acc, rlime_acc))
+        # Sample from the rule.
+        samples, labels = sampler.sample(10000, arm.rule)
+        lime_acc, rlime_acc = calc_accuracy(
+            samples, labels, lime_weights, scaler, arm
+        )
+
+        # Print and save the results.
+        print(f"Process {idx:03d} Finished.")
+        print(f"  LIME  : {lime_acc:.4f}")
+        print(f"  R-LIME: {rlime_acc:.4f}")
+        result_list.append((lime_acc, rlime_acc))
 
 
 if __name__ == "__main__":
